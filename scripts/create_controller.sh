cd907086edbd36b65e778f1000e582b215bad4e8
TheMarlboroMan <marlborometal@gmail.com>
2018-07-23 16:29:47 +0200

    Still more

--- scripts/create_controller.sh
+++ scripts/create_controller.sh
@@ -7,14 +7,12 @@ else
 	recipe_deps="$target:\$(DIR_CONTROLLERS)$1.h \$(DIR_CONTROLLERS)$1.cpp";
 	recipe="	\$(CC) \$(CFLAGS) \$(INCLUDES) \$(DIR_CONTROLLERS)$1.cpp -o $target"
 
-
 	cp class/controllers/_template.h class/controllers/$1.h
 	cp class/controllers/_template.cpp class/controllers/$1.cpp
 	sed -i -e "s/template/$1/g" class/controllers/$1.h
 	sed -i -e "s/TEMPLATE/$uppercasename/g" class/controllers/$1.h
 	sed -i -e "s/template/$1/g" class/controllers/$1.cpp
-	sed -i -e "s/\/*new states go here*\//state_$1, \/*new states go here*\//g" class/controllers/states.h
-#	sed -i -e "s/$ecl/$target $ecl/g" make/controllers
+	sed -i -e "s/state_max/state_$1, state_max/g" class/controllers/states.h
 
 	echo "DEP_CONTROLLERS+= $target" >> make/controllers;
 	echo "$recipe_deps" >> make/controllers;

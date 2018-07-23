018aefac2af4df8d2c35c592751a6e8b52c5ab57
TheMarlboroMan <marlborometal@gmail.com>
2018-07-23 16:47:25 +0200

    Still more

--- scripts/create_controller.sh
+++ scripts/create_controller.sh
@@ -13,7 +13,7 @@ else
 	sed -i -e "s/TEMPLATE/$uppercasename/g" class/controllers/$1.h
 	sed -i -e "s/template/$1/g" class/controllers/$1.cpp
 	sed -i -e "s/state_max/state_$1, state_max/g" class/controllers/states.h
-	awk '{gsub("//register controllers here.", "reg(c_examples, t_states::state_examples, new controller_examples(log));");}'
+	awk '{gsub("//register controllers here.", "reg(c_examples, t_states::state_examples, new controller_examples(log));");}' class/dfwimpl/state_driver.cpp
 
 
 	echo "DEP_CONTROLLERS+= $target" >> make/controllers;

489114fbc143e1c2259e2e090d043b8502338f1a
TheMarlboroMan <marlborometal@gmail.com>
2018-07-23 17:00:04 +0200

    Still more

--- scripts/create_controller.sh
+++ scripts/create_controller.sh
@@ -6,6 +6,8 @@ else
 	target="\$(DIR_O)$1_c.o"
 	recipe_deps="$target:\$(DIR_CONTROLLERS)$1.h \$(DIR_CONTROLLERS)$1.cpp";
 	recipe="	\$(CC) \$(CFLAGS) \$(INCLUDES) \$(DIR_CONTROLLERS)$1.cpp -o $target"
+	awk_register_pattern="//register controllers here";
+	awk_register_replacement="reg(c_$1, t_states::state_$1, new controller_$1(log));\n\t$awk_register_pattern"
 
 	cp class/controllers/_template.h class/controllers/$1.h
 	cp class/controllers/_template.cpp class/controllers/$1.cpp
@@ -13,7 +15,7 @@ else
 	sed -i -e "s/TEMPLATE/$uppercasename/g" class/controllers/$1.h
 	sed -i -e "s/template/$1/g" class/controllers/$1.cpp
 	sed -i -e "s/state_max/state_$1, state_max/g" class/controllers/states.h
-	awk '{gsub("//register controllers here.", "reg(c_examples, t_states::state_examples, new controller_examples(log));");}' class/dfwimpl/state_driver.cpp
+	awk -v p="$awk_register_pattern" "\$0 ~ p {gsub(p, "$awk_register_replacement"p)}1" class/dfwimpl/state_driver.cpp
 
 
 	echo "DEP_CONTROLLERS+= $target" >> make/controllers;

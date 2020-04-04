507a5d6d0c83625001b5bb133e9c8bfea48a2725
TheMarlboroMan <marlborometal@gmail.com>
2020-04-04 21:12:10 +0200

    Tar file for templates and stuff

--- scripts/setup_fresh.sh
+++ scripts/setup_fresh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 current_dir=`pwd | awk -F '/' '{print $NF}'`
 
 if [ "scripts" != "$current_dir" ]
@@ -8,38 +10,22 @@ fi
 
 cd ..
 
-#remove shell scripts and readme file
-rm tiles.sh;
-rm editor.sh;
-rm README.md;
+#remove shell scripts, main and readme file
+rm tiles.sh editor.sh README.md main.cpp
 
-#remove integration, logs, classes, entrypoint...
-rm -rf integration;
-rm -rf logs/*;
-rm -rf class;
-rm -rf data;
-rm -rf class/dfwimpl;
-rm main.cpp;
+#remove classes and dirs
+rm -rf integration logs class data main.cpp
 
 #refresh the directories
-mkdir logs;
-mkdir -p class/dfwimpl class/controllers;
-mkdir -p data/config;
+mkdir -p logs class/dfwimpl class/controllers data/config
 
 #move the blank templates
-mv templates/input.h class;
-mv templates/app_config.* class/dfwimpl;
-mv templates/state_driver.* class/dfwimpl;
-mv templates/states.h class/controllers;
-mv templates/_template.* class/controllers;
-mv templates/config.dnot data/config;
-mv templates/main.cpp ./
-mv templates/GETTING_STARTED.txt .
+tar -xzf templates/templates.tar.gz
+rm -rf templates
 
-#remove templates, git and so on...
-rm -rf templates;
-rm -rf .git;
-rm scripts/setup_fresh.sh;
+#Remove git and scripts
+#rm -rf .git;
+#rm scripts/setup_fresh.sh;
 
-#give a little help...
+#give a little help, which was on the tar file.
 cat GETTING_STARTED.txt;

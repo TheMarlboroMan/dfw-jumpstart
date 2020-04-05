21701bfe76c631a4dc4838f06404d4cb2a37bb0f
TheMarlboroMan <marlborometal@gmail.com>
2020-04-05 20:51:39 +0200

    moving resources around

---
+++ resources/logs/README.md
@@ -0,0 +1,1 @@
+Log files would go here
--- scripts/setup_fresh.sh
+++ scripts/setup_fresh.sh
@@ -33,26 +33,19 @@ then
 	rm -rf build
 fi
 
-#remove shell scripts, main and readme file
-rm tiles.sh editor.sh README.md main.cpp
+#remove the project specifics...
+rm -rf integration logs class data main.cpp tiles.sh editor.sh README.md CMakeLists.txt .git
 
-#remove classes and dirs
-rm -rf integration logs class data main.cpp
+#refresh the directories...
+mkdir -p class/dfwimpl class/controllers
 
-#refresh the directories
-mkdir -p logs class/dfwimpl class/controllers data/config
-
-#move the blank templates
+#decompress the blank templates...
 tar -xzf scripts/templates.tar.gz
 
-#Remove git and scripts
-rm -rf .git;
-rm scripts/setup_fresh.sh;
-rm scripts/make_templates.sh
-rm scripts/templates.tar.gz
+#rename the project
+sed -i s/project-placeholder/$PROJECT/g CMakeLists.txt
 
-#Rename project, create the build directory
-sed -i s/dfw-jumpstart/$PROJECT/g CMakeLists.txt
+#create the build directory
 mkdir build
 cd build
 $CMAKE .. > /dev/null
@@ -62,10 +55,11 @@ then
 	exit 1
 fi
 
-cd ..
-mkdir logs
+#cleanup
+rm scripts/setup_fresh.sh;
+rm scripts/templates.tar.gz
 
 #give a little help, which was on the tar file.
+cd ..
 cat GETTING_STARTED.txt;
-
 exit 0

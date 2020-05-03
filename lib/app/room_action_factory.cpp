b02951d14694369b396355bbc03c77548ea52968
The Marlboro Man <marlborometal@gmail.com>
2020-05-03 18:05:34 +0200

    Fixes

--- lib/app/room_action_factory.cpp
+++ lib/app/room_action_factory.cpp
@@ -5,18 +5,17 @@
 using namespace app;
 
 room_action_factory::room_action_factory(std::vector<std::unique_ptr<room_action>>& a)
-	:actions(a)
-{
+	:actions(a) {
 
 }
 
-void room_action_factory::make_action(const rapidjson::Value& tok)
-{
+void room_action_factory::make_action(const rapidjson::Value& tok) {
+
 	int 	id=tok["id"].GetInt();
 	bool	repeat=tok["repeat"].GetBool();
 
-	switch(tok["type"].GetInt())
-	{
+	switch(tok["type"].GetInt()) {
+
 		case 1: //Room exit
 			actions.emplace_back(new room_action_exit(id, repeat, tok["room"].GetString(), tok["terminus_id"].GetInt()));
 		break;

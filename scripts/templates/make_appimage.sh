ce0510656489cb6c29687a682005ecdbbb0a282e
The Marlboro Man <marlborometal@gmail.com>
2025-09-12 22:03:11 +0200

    Some fixes

--- scripts/templates/make_appimage.sh
+++ scripts/templates/make_appimage.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]
+if [[ $# -ne 3 ]]
 then
-	echo "usage: make_appimage.sh path_to_executable path_to_linuxdeploy"
+	echo "usage: make_appimage.sh path_to_executable path_to_resources path_to_linuxdeploy"
+	echo 'eg: make_appimage.sh build/myexec build/resources $(which linuxdeploy)'
 	exit 1
 fi;
+
+EXECUTABLE_PATH=$1
+RESOURCES_PATH=$2
+LINUXDEPLOY_PATH=$3
 
 #export SIGN=1
 #export SIGN_KEY=0xLAST8CHARSOFGPGKEYID
@@ -13,12 +18,13 @@ MAJOR_VERSION=$(grep "set(MAJOR_VERSION" CMakeLists.txt | awk '{print $2}' | tr 
 MINOR_VERSION=$(grep "set(MINOR_VERSION" CMakeLists.txt | awk '{print $2}' | tr -d ")")
 PATCH_VERSION=$(grep "set(PATCH_VERSION" CMakeLists.txt | awk '{print $2}' | tr -d ")")
 
-export VERSION="$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-echo "will build $VERSION"
-$2 --appdir AppDir
-cp -r resources/* AppDir/usr/share
+export ARCH=x86_64
+export LINUXDEPLOY_OUTPUT_VERSION="$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
+echo "will build $LINUXDEPLOY_OUTPUT_VERSION"
+$LINUXDEPLOY_PATH --appdir AppDir
+cp -r $RESOURCES_PATH AppDir/usr/share
 mkdir -p AppDir/usr/share/metainfo
 cp linuxdeploy/*.xml AppDir/usr/share/metainfo/
-$2 --executable $1 --appdir AppDir -i ./linuxdeploy/icon.png -d ./linuxdeploy/app.desktop --output appimage
+$LINUXDEPLOY_PATH --executable $EXECUTABLE_PATH --appdir AppDir -i ./linuxdeploy/icon.png -d ./linuxdeploy/app.desktop --output appimage
 rm -rf AppDir
 exit 0
